f1f033daf36f4b70e42d223cbd3c6045bc8abd45
Adrian Hartanto <adriandanhartanto004@gmail.com>
2024-10-16 15:26:59 +0700

    Initial Commit

---
+++ iosMovieapp/iosMovieappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosMovieappApp.swift
+//  iosMovieapp
+//
+//  Created by Adrian Hartanto on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iosMovieappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
